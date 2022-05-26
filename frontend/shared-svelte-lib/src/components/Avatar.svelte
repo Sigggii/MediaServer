24e7f97246b0fc000979b74b9b55f78ca5f73bbe
Sigggii <simeon-holzapfel@gmx.de>
2022-05-26 15:10:50 +0200

    shared svelte package for webapp and later mobile app

---
+++ frontend/shared-svelte-lib/src/components/Avatar.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let src: string;
+	export let pixelWidth: number;
+</script>
+
+<div style="width: {pixelWidth + 'px'}">
+	<img {src} alt="Avatar" />
+</div>
+
+<style>
+	img {
+		width: 100%;
+		border-radius: 50%;
+	}
+</style>
--- frontend/webapp/src/routes/index.svelte
+++ frontend/webapp/src/routes/index.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { media } from '../base/stores/media.js'
+	import Avatar from 'shared-svelte-lib/src/components/Avatar.svelte'
+	const url = 'https://vampster.com/wp-content/uploads/Lord-Of-The-Lost-Judas-800x750.jpg'
+</script>
+
+{#if $media.xl}
+	<Avatar src={url} pixelWidth={300} />
+{:else}
+	<Avatar src={url} pixelWidth={100} />
+{/if}
